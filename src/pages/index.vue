9cbfcdd1bc9e094737fa616471239eb7c8efefd4
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-04-13 01:30:08 +0530

    chore: add a header for the index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,24 +1,15 @@
 <template>
-  <header>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra.
-  </header>
-  <section>
-    <!-- Example blog -->
-    <div>
-      <h1>Lorem ipsum dolor sit amet</h1>
-      <p>
-        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit
-      </p>
-      <hr />
-      <span>Somraj Saha</span>
-      <span>{{
-        new Date().toLocaleDateString("en-IN", {
-          day: "2-digit",
-          month: "short",
-          year: "numeric",
-        })
-      }}</span>
-    </div>
-  </section>
+  <h1>THE BLOG</h1>
 </template>
+
+<style lang="scss" scoped>
+h1 {
+  font-style: bold;
+  font-size: 15.2375rem;
+  text-align: center;
+  color: #fff;
+  border-top: 1px solid white;
+  border-bottom: 1px solid white;
+  margin-top: 2rem;
+}
+</style>
