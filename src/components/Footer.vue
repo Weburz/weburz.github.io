f55c2a093cdf017cde8b3966d22f8cd9130ebad6
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-04-13 01:13:02 +0530

    chore: refactor the layout components

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <small>
+      Weburz LLC &copy; 2024-{{ new Date().getFullYear() }} &middot; Made With
+      Open-Source ❤️
+    </small>
+  </footer>
+</template>
+
+<script setup lang="ts">
+console.log("Hello World!");
+</script>
+
+<style lang="scss" scoped>
+small {
+  color: #fff;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <nav>
+      <span>Tech at Weburz</span>
+      <ol class="nav-items">
+        <li>
+          <NuxtLink href="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink href="/blog">Blog</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink href="/projects">Projects</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink href="/technology">Technology</NuxtLink>
+        </li>
+      </ol>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+console.log("Hello World!");
+</script>
+
+<style lang="scss" scoped>
+nav {
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+
+  .nav-items {
+    display: flex;
+    gap: 1.75rem;
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+}
+</style>
